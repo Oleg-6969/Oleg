2c313d055d269d67f52d134c5426469f34b6c224
Oleg-6969 <petrukoleg038@gmail.com>
2025-09-10 10:38:59 +0300

    Update Олег2.py

--- Олег2.py
+++ Олег2.py
@@ -2,25 +2,25 @@ list=[12, 23, 43, 565,2, 5, 6, 4, 0, 1, 'banana', 'apple', 'melon', 'pear', 'plu
 numbers = []
 letters = []
 for item in a:
-  if type(item) is stp:
+  if type(item) is str:
     letters.append(item)
   elif type(item) is int:
       numbers.append(item)
 
-number.sort()
+numbers.sort()
 letters.sort()
 
 fin_list= numbers + letters
 
 two=[]
-for item in numbers
-    if item % 2 == 0
+for item in numbers:
+    if item % 2 == 0:
         two.append(item)
 
 caps = []
-for item in letters
+for item in letters:
      caps.append(item.upper())
 
-prinr('відсортований список', fin_list)
-print('список парних чисел' two)
+print('відсортований список', fin_list)
+print('список парних чисел', two)
 print('срисок літер написаних капсом', caps)
